914df45fbcdb3cc50f80fd151d631469dded8f13
yusufnadiruzun <yusuf.nadir94@gmail.com>
2022-12-02 14:58:45 +0300

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+
+      <Text>asd</Text>
+    
+ 
+    </ScrollView>
+    
   );
 }
 
